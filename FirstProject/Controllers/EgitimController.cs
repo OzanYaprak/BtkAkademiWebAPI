4053c603dba4f8a8ecb7fea800b111211029f619
Ozan YAPRAK <ozanerhanyaprak@icloud.com>
2025-03-26 15:09:39 +0300

    Add project files.

---
+++ FirstProject/Controllers/EgitimController.cs
@@ -0,0 +1,21 @@
+﻿using FirstProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstProject.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class EgitimController : ControllerBase
+    {
+        [HttpGet("TestMethod")]
+        public ResponseModel TestMethod()
+        {
+            return new ResponseModel()
+            {
+                StatusCode = 200,
+                Description = "Başarılı",
+            };
+        }
+    }
+}
---
+++ FirstProject/Models/ResponseModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FirstProject.Models
+{
+    public class ResponseModel
+    {
+        public int StatusCode { get; set; }
+        public string Description { get; set; }
+    }
+}
