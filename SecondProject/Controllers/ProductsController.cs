aa8c3bc1a556ee25e267ceb6b2a52f7b69bba7e4
Ozan YAPRAK <ozanerhanyaprak@icloud.com>
2025-03-26 15:38:58 +0300

    #

---
+++ SecondProject/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using SecondProject.Models;
+
+namespace SecondProject.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(ILogger<ProductsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            var products = new List<Product>()
+            {
+                new Product { Id = 1,ProductName="Elma" },
+                new Product { Id = 2,ProductName="Karpuz" },
+                new Product { Id = 3,ProductName="Limon" }
+            };
+
+            _logger.LogInformation("GetAllProducts Action Has Been Called.");
+
+            return Ok(products);
+        }
+    }
+}
---
+++ SecondProject/Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace SecondProject.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+    }
+}
