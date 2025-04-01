1e65304bbb0214d4156c10dc0f68b221f3d47274
Ozan YAPRAK <ozanerhanyaprak@icloud.com>
2025-04-01 22:25:47 +0300

    #

---
+++ WebApi/Repositories/ApplicationContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.Repositories
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+}
